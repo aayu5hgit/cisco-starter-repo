a56283121d5e8dfa15cc567a3f196895362f52f9
AAYUSH TALREJA <86314754+aayu5hgit@users.noreply.github.com>
2022-08-09 14:08:53 +0530

    UI

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
+import Welcome from './Components/Welcome';
 
 function App() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg fixed-top" style={{backgroundColor: 'transparent'}}>
+      <nav className="navbar navbar-expand-lg bg-light fixed-top">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">Sextant</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{marginLeft: 450}}>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">Home</a>
@@ -35,5 +36,7 @@ function App() {
     </div>
   );
 }
+<Welcome/>
+
 
 export default App;
---
+++ sextant/src/Components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function welcome(props) {
+    return (  
+        <div>
+        Welcome
+      </div>
+    );
+}
+
+export default welcome;
